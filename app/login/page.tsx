b1d39a0b09ee31c0f76fd6091fd458ccd7de6821
ArkRavi <gitproject@arkmedis.com>
2025-07-14 22:36:20 +0530

    pushing chnages to create client page

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -11,6 +11,7 @@ export default function LoginPage() {
 
   const handleLogin = async (email: string, password: string) => {
     const supabase = createClient();
+       
     const { error } = await supabase.auth.signInWithPassword({
       email,
       password,
--- lib/supabase/client.ts
+++ lib/supabase/client.ts
@@ -1,8 +1,15 @@
-import { createBrowserClient } from '@supabase/ssr';
+// import { createBrowserClient } from '@supabase/ssr';
+// //import { createBrowserClient } from '@supabase/auth-helpers-nextjs';
+
+
+// export function createClient() {
+//   return createBrowserClient(
+//     process.env.NEXT_PUBLIC_SUPABASE_URL!,
+//     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+//   );
+// }
+import { createPagesBrowserClient } from '@supabase/auth-helpers-nextjs';
 
 export function createClient() {
-  return createBrowserClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
-  );
+  return createPagesBrowserClient();
 }
